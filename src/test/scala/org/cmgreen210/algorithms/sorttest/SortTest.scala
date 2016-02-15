4bdb6cdf216001dc97d78042e7e1a5def3d42a9c
Chris Green <cmgreen210@gmail.com>
2016-02-15 10:04:57 -0800

    Selection Sort

--- src/test/scala/org/cmgreen210/algorithms/sorttest/SortTest.scala
+++ src/test/scala/org/cmgreen210/algorithms/sorttest/SortTest.scala
@@ -8,18 +8,27 @@ import org.scalatest.exceptions.TestFailedDueToTimeoutException
 import org.scalatest.time.SpanSugar._
 
 
-class SpeedFailTest extends FlatSpec with Timeouts {
+class SpeedTest extends FlatSpec with Timeouts {
 
   lazy val SPEED_FAIL = 100 millis
   lazy val arr: Array[Int] = loadArray("arr10000.csv")
 
-  def run(algo: Sort): Unit = {
+  private def run(algo: Sort): Unit = {
+    failAfter(SPEED_FAIL) {
+      algo.sort(arr)
+    }
+  }
+
+  def fail(algo: Sort): Unit = {
     it should "timeout" in {
       intercept[TestFailedDueToTimeoutException] {
-        failAfter(SPEED_FAIL) {
-          algo.sort(arr)
-        }
+        run(algo)
       }
+    }
+  }
+  def pass(algo: Sort): Unit = {
+    it should "pass" in {
+      run(algo)
     }
   }
 }
@@ -35,8 +44,8 @@ class InsertionSortTest extends FlatSpec with Sort with Timeouts with Matchers {
   }
 
   it should "catch a timeout exception because insertion sort is slow!" in {
-    val speedFail = new SpeedFailTest
-    speedFail.run(this)
+    val speedFail = new SpeedTest
+    speedFail.fail(this)
   }
 
   it should "show stability" in {
@@ -55,8 +64,8 @@ class SelectionSortTest extends FlatSpec with Sort with Timeouts with Matchers {
   }
 
   it should "catch a timeout exception because insertion sort is slow!" in {
-    val speedFail = new SpeedFailTest
-    speedFail.run(this)
+    val speedFail = new SpeedTest
+    speedFail.fail(this)
   }
 
   it should "show instability" in {
